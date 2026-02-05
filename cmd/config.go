bdcfed089c585d75df129a009cc35b9b85ec077f
Randy Deborggraeve <randy@deb-ict.com>
2026-02-05 22:24:44 +0100

    Refactor main

---
+++ cmd/config.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/deb-ict/cloudbm-community/pkg/hosting"
+	"gopkg.in/yaml.v3"
+
+	auth_svc "github.com/deb-ict/cloudbm-community/pkg/module/auth/service"
+	contact_svc "github.com/deb-ict/cloudbm-community/pkg/module/contact/service"
+	gallery_svc "github.com/deb-ict/cloudbm-community/pkg/module/gallery/service"
+	product_svc "github.com/deb-ict/cloudbm-community/pkg/module/product/service"
+	session_svc "github.com/deb-ict/cloudbm-community/pkg/module/session/service"
+)
+
+type config struct {
+	Http                  hosting.HttpConfig         `yaml:"http"`
+	AuthenticationService auth_svc.ServiceOptions    `yaml:"authentication_service"`
+	ContactService        contact_svc.ServiceOptions `yaml:"contact_service"`
+	GalleryService        gallery_svc.ServiceOptions `yaml:"gallery_service"`
+	ProductService        product_svc.ServiceOptions `yaml:"product_service"`
+	SessionService        session_svc.ServiceOptions `yaml:"session_service"`
+}
+
+func LoadConfig(configPath string) (*config, error) {
+	cfg := &config{
+		Http:                  hosting.HttpConfig{},
+		AuthenticationService: auth_svc.ServiceOptions{},
+		ContactService:        contact_svc.ServiceOptions{},
+		GalleryService:        gallery_svc.ServiceOptions{},
+		ProductService:        product_svc.ServiceOptions{},
+		SessionService:        session_svc.ServiceOptions{},
+	}
+	err := cfg.loadYaml(configPath)
+	cfg.loadEnvironment()
+	cfg.ensureDefaults()
+	return cfg, err
+}
+
+func (cfg *config) loadYaml(configPath string) error {
+	logger := slog.With(slog.String("configPath", configPath))
+	logger.InfoContext(context.Background(), "Loading config")
+
+	// Open the config file
+	if _, err := os.Stat(configPath); err == nil {
+		file, err := os.Open(configPath)
+		if err != nil {
+			logger.ErrorContext(context.Background(), "Failed to open config",
+				slog.Any("error", err),
+			)
+			return err
+		}
+		defer file.Close()
+
+		// Read the config file
+		decoder := yaml.NewDecoder(file)
+		err = decoder.Decode(cfg)
+		if err != nil {
+			logger.ErrorContext(context.Background(), "Failed to parse config",
+				slog.Any("error", err),
+			)
+			return err
+		}
+	} else if len(configPath) > 0 {
+		logger.WarnContext(context.Background(), "Failed to load config",
+			slog.Any("error", err),
+		)
+	}
+
+	return nil
+}
+
+func (cfg *config) loadEnvironment() {
+	cfg.Http.LoadEnvironment()
+}
+
+func (cfg *config) ensureDefaults() {
+	cfg.Http.EnsureDefaults()
+	cfg.AuthenticationService.EnsureDefaults()
+	cfg.ContactService.EnsureDefaults()
+	cfg.GalleryService.EnsureDefaults()
+	cfg.ProductService.EnsureDefaults()
+	cfg.SessionService.EnsureDefaults()
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-	"log"
+	"context"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
 	auth_api_v1 "github.com/deb-ict/cloudbm-community/pkg/module/auth/api/v1"
@@ -19,35 +23,93 @@ import (
 )
 
 func main() {
-	r := mux.NewRouter().StrictSlash(true)
+	// Parse arguments
+	var configPath string
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.StringVar(&configPath, "config", "/etc/cloudbm/portal.conf", "the path of the configuration file")
+	flag.Parse()
 
-	authSvc := auth_svc.NewService(nil, &auth_svc.ServiceOptions{})
-	authApiV1 := auth_api_v1.NewApiV1(authSvc)
-	authApiV1.RegisterRoutes(r.PathPrefix("auth").Subrouter())
+	// Setup the default logger
+	slogJsonHandler := slog.NewJSONHandler(os.Stdout, nil)
+	slog.SetDefault(slog.New(slogJsonHandler))
+	slog.SetLogLoggerLevel(slog.LevelInfo)
 
-	gallerySvc := gallery_svc.NewService(nil, &gallery_svc.ServiceOptions{})
-	galleryApiV1 := gallery_api_v1.NewApiV1(gallerySvc)
-	galleryApiV1.RegisterRoutes(r.PathPrefix("gallery").Subrouter())
+	// Load configuration
+	config, err := LoadConfig(configPath)
+	if err != nil {
+		os.Exit(1)
+	}
 
-	contactSvc := contact_svc.NewService(nil, &contact_svc.ServiceOptions{})
-	contactApiV1 := contact_api_v1.NewApiV1(contactSvc)
-	contactApiV1.RegisterRoutes(r.PathPrefix("contact").Subrouter())
+	// Handle SIGINT (CTRL+C) gracefully.
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
 
-	productSvc := product_svc.NewService(nil, &product_svc.ServiceOptions{})
-	productApiV1 := product_api_v1.NewApiV1(productSvc)
-	productApiV1.RegisterRoutes(r.PathPrefix("product").Subrouter())
+	// Setup the HTTP server and routes
+	router := mux.NewRouter().StrictSlash(true)
+	initServices(router)
 
-	sessionSvc := session_svc.NewService(nil, &session_svc.ServiceOptions{})
-	sessionApiV1 := session_api_v1.NewApiV1(sessionSvc)
-	sessionApiV1.RegisterRoutes(r.PathPrefix("session").Subrouter())
-
-	srv := &http.Server{
-		Handler:      r,
-		Addr:         "127.0.0.1:8000",
+	// Start the HTTP server
+	httpServerAddr := config.Http.GetBindAddress()
+	slog.InfoContext(context.Background(), "Starting http server",
+		slog.String("address", httpServerAddr),
+	)
+	httpServer := &http.Server{
+		Handler:      router,
+		Addr:         httpServerAddr,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
+	httpServerErr := make(chan error, 1)
+	go func() {
+		httpServerErr <- httpServer.ListenAndServe()
+	}()
 
-	//TODO: We need slog logging here
-	log.Fatal(srv.ListenAndServe())
+	// Wait for interruption.
+	select {
+	case err := <-httpServerErr:
+		// Error when starting HTTP server.
+		slog.ErrorContext(context.Background(), "Error while running http server",
+			slog.Any("error", err),
+		)
+	case <-ctx.Done():
+		// Wait for first CTRL+C.
+		// Stop receiving signal notifications as soon as possible.
+		slog.InfoContext(context.Background(), "Application termination requested")
+		stop()
+	}
+
+	// Stop http server
+	slog.InfoContext(context.Background(), "Stopping http server")
+	httpServer.Shutdown(ctx)
+
+	// Shutdown
+	os.Exit(0)
+}
+
+func initServices(router *mux.Router) {
+	//TODO: Database connection
+	authSvc := auth_svc.NewService(nil, &auth_svc.ServiceOptions{})
+	authApiV1 := auth_api_v1.NewApiV1(authSvc)
+	authApiV1.RegisterRoutes(router.PathPrefix("auth").Subrouter())
+
+	//TODO: Database connection
+	gallerySvc := gallery_svc.NewService(nil, &gallery_svc.ServiceOptions{})
+	galleryApiV1 := gallery_api_v1.NewApiV1(gallerySvc)
+	galleryApiV1.RegisterRoutes(router.PathPrefix("gallery").Subrouter())
+
+	//TODO: Database connection
+	contactSvc := contact_svc.NewService(nil, &contact_svc.ServiceOptions{})
+	contactApiV1 := contact_api_v1.NewApiV1(contactSvc)
+	contactApiV1.RegisterRoutes(router.PathPrefix("contact").Subrouter())
+
+	//TODO: Database connection
+	productSvc := product_svc.NewService(nil, &product_svc.ServiceOptions{})
+	productApiV1 := product_api_v1.NewApiV1(productSvc)
+	productApiV1.RegisterRoutes(router.PathPrefix("product").Subrouter())
+
+	//TODO: Database connection
+	sessionSvc := session_svc.NewService(nil, &session_svc.ServiceOptions{})
+	sessionApiV1 := session_api_v1.NewApiV1(sessionSvc)
+	sessionApiV1.RegisterRoutes(router.PathPrefix("session").Subrouter())
 }
